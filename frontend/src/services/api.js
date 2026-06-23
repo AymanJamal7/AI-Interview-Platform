e5c9f7642fde96fa31d7b5c66c1c6970e93f47ae
Ayman Jamal <aymanjamal7287@gmail.com>
2026-06-23 15:45:01 +0530

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5200/api",
+  baseURL: "https://ai-interview-platform-snaz.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
